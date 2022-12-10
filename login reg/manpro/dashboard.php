d21e8b216b2cb6f5b7b90928379426302c408ee7
LAPTOP-QIQ4NRNI\LENOVO <darrukk01@gmail.com>
2022-12-10 22:10:19 +0700

    coba

---
+++ login reg/manpro/dashboard.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard</title>
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- style -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="con">
+        <div class="sidebar">
+            <div class="header">
+                <div class="list-item">
+                    <a href="#">
+                    <span class="description-header">Zekola</span>
+                    </a>
+                </div>
+                <div class="ilustrasi">
+                    <img src="asset/WhatsApp_Image_2022-09-29_at_9.12 1.svg" alt="">
+
+                </div>
+            </div>
+            <div class="main">
+                <div class="list-item">
+                    <a href="#">
+                        <img src="" alt="" class="icon">
+                        <span class="description"></span>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+        <div class="main-content">
+
+        </div>
+
+    </div>
+
+    <script src="script.js"></script>
+    
+</body>
+</html>
